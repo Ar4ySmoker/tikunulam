9dcbfdfdc8e90b4e3c573b8f5ac275f25708d2fe
Artur Fedcenco <developerar4y@gmail.com>
2023-07-13 00:03:35 +0300

    vspominal

--- src/components/FourthSect/FourthSect.js
+++ src/components/FourthSect/FourthSect.js
@@ -1,43 +1,63 @@
-import App from "../../App";
+const threeSectObjectsLT = [
+  { text: "Докуметны >" },
+  { text: "Документация нашей фирмы: NIP,WAT и другие >" },
+  { text: "Подробнее >" },
+];
+const threeSectObjectsRT = [
+  { text: "Отправка" },
+  { text: "Vander Elst visa,переезд,зарплата и другие вопросы" },
+  { text: "Ознакомиться с деталями" },
+];
+
+const threeSectObjectsLB = [
+  { text: "Новости >" },
+  { text: "Перезд,работа,быт >" },
+  { text: "Читать новости >" },
+];
+const threeSectObjectsRB = [
+  { text: "Контакты >" },
+  { text: "ыстрый отклик по любой связи >" },
+  { text: "Перейти >" },
+];
+
 function FourthSect() {
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-4">
       <div className=" p-4 h-[84vh] shadow-fourth">
-        <div>
-          <h4>Докуметны</h4>
-          <h5>Документация нашей фирмы: NIP,WAT и другие</h5>
-          <a href="">Подробнее </a>
-        </div>
+        <ul id="">
+          {threeSectObjectsLT.map((item) => (
+            <li className="cursor-pointer text-cyan-600" key={item}>
+              <a href={item.link}>{item.text}</a>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="p-4 h-[84vh] ">
-        <div id="">
-          <h4>Отправка</h4>
-          <h5>vander Elst visa,переезд,зарплата и другие вопросы</h5>
-          <a href="">Ознакомиться с деталями </a>
-        </div>
+        <ul id="">
+          {threeSectObjectsRT.map((item) => (
+            <li className="cursor-pointer text-cyan-600" key={item}>
+              <a href={item.link}>{item.text}</a>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className=" h-[84vh] shadow-fourth 	">
-        <div className="">
-          <h4>Новости</h4>
-          <h5>Перезд,работа,быт.</h5>
-          <div class="">
-            <a id="" href="">
-              Читать новости{" "}
-            </a>
-            <a id="" href="">
-              Подписаться{" "}
-            </a>
-          </div>
-        </div>
+        <ul id="">
+          {threeSectObjectsLB.map((item) => (
+            <li className="cursor-pointer text-cyan-600" key={item}>
+              <a href={item.link}>{item.text}</a>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className=" p-4 h-[84vh]  ">
-        <div class="">
-          <h4>Контакты</h4>
-          <h5>Быстрый отклик по любой связи.</h5>
-          <div class="">
-            <a href="">Перейти </a>
-          </div>
-        </div>
+        <ul id="">
+          {threeSectObjectsRB.map((item) => (
+            <li className="cursor-pointer text-cyan-600" key={item}>
+              <a href={item.link}>{item.text}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
